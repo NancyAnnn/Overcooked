3c139b7f485a61ea9e46e060fc377a50469f843f
NancyAnnn <2360925002@qq.com>
2026-09-19 16:09:25 +0800

    shader

--- Assets/Scripts/Counters/ClearCounter.cs
+++ Assets/Scripts/Counters/ClearCounter.cs
@@ -20,8 +20,21 @@ public class ClearCounter : BaseCounter {
             // There is a KitchenObject here
             if (player.HasKitchenObject()) {
                 // Player is carrying something
-            }
-            else {
+                if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject)) {
+                    // Player is holding a plate
+                    if (plateKitchenObject.TryAddIngredient(GetKitchenObject().GetKitchenObjectSO())) {
+                        GetKitchenObject().DestroySelf();
+                    }
+                } else {
+                    // Player is not holding a plate but something else
+                    if (GetKitchenObject().TryGetPlate(out plateKitchenObject)) {
+                        // Counter is holding a plate
+                        if (plateKitchenObject.TryAddIngredient(player.GetKitchenObject().GetKitchenObjectSO())) {
+                            player.GetKitchenObject().DestroySelf();
+                        }
+                    }
+                }
+            } else {
                 // Player is not carrying anything
                 GetKitchenObject().SetKitchenObjectParent(player);
             }
